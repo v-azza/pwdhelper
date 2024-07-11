db1303552ee6697916674e4ad206f7259160ba70
v-azza <vazza54@googlemail.com>
2024-07-11 19:57:53 +0100

    add last passwords generated function, added ability for old functions to return their values for later use

--- pwd.py
+++ pwd.py
@@ -30,39 +30,58 @@ def generate_password_characters(n_of_digits, n_punctuation_char, n_passwords, p
     return passwords
 
 
+def create_standard_password(): #function to generate password for OPTION 1, for given user inputs
+    
+    n_of_digits =  int(input("How many numbers 0-9 does each of your passwords require? "))
+    n_punctuation_char = int(input("How many special characters does each of your passwords require? "))
+    n_passwords = int(input("How many passwords do you require? "))
+    password_length = int(input("Please give the password length required: "))
+    
+    if n_of_digits + n_punctuation_char > password_length:
+        print("Error: The sum of digits and special characters exceeds the password length.")
+        return None
+    else:
+        passwords = generate_password_characters(n_of_digits, n_punctuation_char, n_passwords, password_length)
+        for index, password in enumerate(passwords):
+            print(f"Password {index + 1} generated: {password}")
+        return passwords
+
+
+def create_passphrase(): #function to generate passphrase for OPTION 2
+    print("Passphrase function!")
+
 # Main function to handle user choices
 def main():
+    
+    last_generated_passwords = None 
 
     while True:
         print("")
         print("\n1. Create standard password")
         print("2. Create passphrase")
         print("3. Print last password(s) generated")
-        print("4. Exit")
+        print("4. Copy password(s) generated to your clipboard")
+        print("5. Exit")
 
         choice = input("\nEnter your choice (1-4): ").strip()
         
         if choice == "1":
-            #User input
-            n_of_digits =  int(input("How many numbers 0-9 does each of your passwords require? "))
-            n_punctuation_char = int(input("How many special characters does each of your passwords require? "))
-            n_passwords = int(input("How many passwords do you require? "))
-            password_length = int(input("Please give the password length required: "))
-            
-            if n_of_digits + n_punctuation_char > password_length:
-                print("Error: The sum of digits and special characters exceeds the password length.")
-            else:
-                passwords = generate_password_characters(n_of_digits, n_punctuation_char, n_passwords, password_length)
-                for index, password in enumerate(passwords):
-                    print(f"Password {index + 1} generated: {password}")
+            last_generated_passwords = create_standard_password()
         
         elif choice == "2":
-            print("Passphrase generation not implemented yet.")
+            create_passphrase()
             
         elif choice == "3":
-            print("Not built yet.")
+            if last_generated_passwords:
+                for index, password in enumerate(last_generated_passwords):
+                    print(f"Password {index + 1} generated: {password}")
+            else:
+                print("No passwords generated yet.")
             
         elif choice == "4":
+            print("Not built yet.")
+            
+        elif choice == "5":
             print("Quitting...")
             break
         
