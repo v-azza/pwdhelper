dc1e7e9c1361014f4918478f290ce95101967a11
v-azza <vazza54@googlemail.com>
2024-07-24 07:52:43 +0100

    pwd-comment-update

--- pwd.py
+++ pwd.py
@@ -1,6 +1,10 @@
 import random, string
 
-#Location for eff wordlist: https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt
+"""
+A small utility to help you generate passwords locally, through randomly generated characters or a passphrase. This can be used offline. Passphrase generation is done through a publicly available EFF wordlist.
+
+Location for eff wordlist: https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt
+"""
 
 #function to convert the password string into list, then apply random.shuffle from random module to prevent min_digit_index and min_punctuation_index outputs from being added sequentially into the final password output
 def randomize_password(password): 
@@ -8,7 +12,7 @@ def randomize_password(password):
     random.shuffle(password_list)
     return "".join(password_list)
 
-
+#function to define the set of characters that will be generated when OPTION 1 is selected
 def generate_password_characters(n_of_digits, n_punctuation_char, n_passwords, password_length):
     char = string.ascii_letters #define the usable set of characters for password from string module
     passwords = []
@@ -27,10 +31,10 @@ def generate_password_characters(n_of_digits, n_punctuation_char, n_passwords, p
 
         passwords.append(randomize_password(password))    
 
-    return passwords
+    return passwords 
 
-
-def create_standard_password(): #function to generate password for OPTION 1, for given user inputs
+#function to generate password for OPTION 1, for given user inputs
+def create_standard_password(): 
     
     n_of_digits =  int(input("How many numbers 0-9 does each of your passwords require? "))
     n_punctuation_char = int(input("How many special characters does each of your passwords require? "))
@@ -46,19 +50,18 @@ def create_standard_password(): #function to generate password for OPTION 1, for
             print(f"Password {index + 1} generated: {password}")
         return passwords
 
-
+#function to read the wordlist file and process the file into a Python list
 def read_wordlist(filepath):
     with open(filepath, 'r') as file:
         words = [line.split()[1] for line in file] #checking for each word in the wordlist
     return words
 
-
-def create_passphrase(wordlist, n_words=4): #function to generate passphrase for OPTION 2
+#function to generate passphrase for OPTION 2, for given user inputs
+def create_passphrase(wordlist, n_words=4): 
     passphrase = ''.join(random.choice(wordlist) for _ in range(n_words))
     return passphrase
 
-
-# Main function to handle user choices
+#main function to handle user choices
 def main():
     wordlist_filepath = 'eff_large_wordlist.txt'
     wordlist = read_wordlist(wordlist_filepath)
@@ -72,7 +75,7 @@ def main():
         print("4. Copy password(s) generated to your clipboard")
         print("5. Exit")
 
-        choice = input("\nEnter your choice (1-4): ").strip()
+        choice = input("\nEnter your choice (1-5): ").strip()
         
         if choice == "1":
             last_generated_passwords = create_standard_password()
@@ -100,6 +103,6 @@ def main():
         else:
             print("Invalid choice. Please try again.")
 
-
+#if statement to let this utility become importable as a module, and executable as a standalone script
 if __name__ == "__main__":
     main() # type: ignore
